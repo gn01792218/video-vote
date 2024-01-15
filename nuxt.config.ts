503e6f1cac7fe33afa939cb24333d0308ab4f497
MSI\gn01792218 <gn01792218@hotmail.com>
2024-01-15 15:29:55 +0800

    [feature] add NuxtUI

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,8 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules:[
-    '@nuxtjs/tailwindcss',
-    '@pinia/nuxt'
+    // '@nuxtjs/tailwindcss',
+    '@pinia/nuxt',
+    '@nuxt/ui'
   ]
 })
